4dc43e619ad5b21e7d8e0d9ae69403846b80fc90
Aditi Bhatnagar <201201164@daiict.ac.in>
2018-04-11 06:38:27 +0530

    Implemented trie, added Cmdlet, handled IO

---
+++ Duh.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Duh {
+
+    [Cmdlet(VerbsCommon.Get, "duh")]
+    public class Duh : Cmdlet {
+        // Declare the parameters for the cmdlet.
+        //[Parameter(Mandatory = true)]
+        //public string InputCommand {
+        //    get { return input_command; }
+        //    set { input_command = value; }
+        //}
+        //private string input_command;
+
+        // Overide the ProcessRecord method to process
+        // the supplied user name and write out a 
+        // greeting to the user by calling the WriteObject
+        // method.
+        protected override void ProcessRecord() {
+            string input;
+            IEnumerable<PSObject> res;
+            //var history = PowerShell.Create().AddCommand("get-history").Invoke();
+            WriteObject("Lets get auto-complete suggestions");
+            using (PowerShell ps = PowerShell.Create(RunspaceMode.CurrentRunspace)) {
+                res = ps.AddCommand("Get-History")
+                        .AddParameter("Count", 1)
+                        .Invoke();
+                input = res.FirstOrDefault().Properties.ElementAt(1).Value.ToString();
+
+            }
+
+            var answer = Trie.GetAutocompleteSuggestions(input).FirstOrDefault();
+            SendKeys.SendWait("Whats up dude, hey jude");
+        }
+    }
+
+    public class ModuleInitializer : IModuleAssemblyInitializer {
+        public void OnImport() {
+            var trieDictionary = new HashSet<string>();
+            int ignoreLineCount = 2;
+            var assembly = Assembly.GetExecutingAssembly();
+            var stdGitCommandsFile = "GitCommonStdCommands.txt";
+            var commandHistoryFile = "CommandHistory.csv";
+            var resourcePrefix = "Duh.Resources.";
+
+            //Loading up all the default standard Git Commands in the trie
+            using (Stream stream = assembly.GetManifestResourceStream(string.Concat(resourcePrefix,stdGitCommandsFile)))
+            using (StreamReader reader = new StreamReader(stream)) {
+                while (reader.Peek() != -1) {
+                    var val = reader.ReadLine();
+                    trieDictionary.Add(val);
+                }
+            }
+            using (Stream stream = assembly.GetManifestResourceStream(string.Concat(resourcePrefix, commandHistoryFile)))
+            using (StreamReader reader = new StreamReader(stream)) {
+                while (reader.Peek() != -1) {
+                    if (ignoreLineCount > 0) {
+                        ignoreLineCount--;
+                        reader.ReadLine();
+                        continue;
+                    }
+                    var val = reader.ReadLine().Split(',')[1].Replace("\"", "");
+                    trieDictionary.Add(val);
+                }
+            }
+
+            Trie.LoadTrie(trieDictionary);
+
+        }
+    }
+}
---
+++ Trie.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Duh {
+    static class Trie {
+        public static Node _root { get; }
+        static HashSet<string> trieDictionary ;
+        static Trie() {
+            _root = new Node(null);
+        }
+
+        public static void LoadTrie(IEnumerable words) {
+            var character = new char();
+            foreach (string word in words) {
+                var charArray = word.ToCharArray();
+                var node = _root;
+                for (var index = 0; index < charArray.Length; index++) {
+                    character = word[index];
+                    if (node.LinkedNodes.ContainsKey(character)) {
+                        node.LinkedNodes.TryGetValue(character, out node);
+                    }
+                    else {
+                        var newNode = new Node(node.Value + character.ToString());
+                        node.LinkedNodes.Add(character, newNode);
+                        node = newNode;
+                    }
+                }
+                node.IsTerminal = true;
+            }
+        }
+
+
+        /* Cases: (1) Missing letters: first letters missing, for every letter, if not found in the 
+             * immediate linked nodes try two subsequent nodes. Must end with terminal node, might match termial true
+             * (2) Extra letters: might not match terminal true
+             * 
+             * 
+             * */
+        //public IEnumerable<string> FindClosestMatch(string word) {
+        //    var k = 1; // number of missing or extra letters allowed including start/mid
+        //    var letters = word.ToCharArray();
+        //    var result = Enumerable.Empty<string>();
+        //    int mlF = 0; //missing letter flag
+        //    int elF = 0; //extra letter flag
+
+        //    var candidate_nodes = new List<Node>();
+        //    candidate_nodes = _root.LinkedNodes.Values.ToList() ;
+
+        //    foreach(var letter in letters) {
+        //        if (node.LinkedNodes.ContainsKey(letter)){
+        //            node.LinkedNodes.TryGetValue(letter, out node);
+        //        }
+        //        else {
+        //            jumpFlag++;
+        //            if(jumpFlag <=k) {
+        //                //check the letter in candidate nodes
+
+        //            }
+        //        }
+        //    }
+
+        //}
+
+
+        //Levenshtein distance
+        //http://people.cs.pitt.edu/~kirk/cs1501/Pruhs/Spring2006/assignments/editdistance/Levenshtein%20Distance.htm
+
+        public static int[] CalculateNextRow(string inputStr, int[] upperRow, char c) {
+            var left = upperRow[0] + 1;
+            var top = 0;
+            var diagonal = upperRow[0];
+
+            int[] resultRow = new int[upperRow.Length];
+            resultRow[0] = left;
+            for (var index = 1; index < upperRow.Length; index++) {
+                top = upperRow[index];
+                resultRow[index] = Math.Min(left + 1, Math.Min(top + 1, diagonal + (c == inputStr.ToCharArray()[index - 1] ? 0 : 1)));
+                left = resultRow[index];
+                diagonal = top;
+            }
+            return resultRow;
+        }
+
+        public static int GetEditDistance(int[] arr) {
+            return arr[arr.Length - 1];
+        }
+        public static bool Search(string input) {
+            var node = _root;
+            var word = input.ToCharArray();
+            foreach (var letter in word) {
+                if (!node.LinkedNodes.ContainsKey(letter))
+                    return false;
+                node.LinkedNodes.TryGetValue(letter, out node);
+            }
+
+
+            if (node.IsTerminal) return true;
+            return false;
+        }
+
+        public static IEnumerable<string> GetAutocompleteSuggestions(string input) {
+            var results = Enumerable.Empty<string>();
+            var node = _root;
+            var phrase = input.ToCharArray();
+            foreach (var letter in phrase) {
+                if (!node.LinkedNodes.ContainsKey(letter))
+                    return results;
+                node.LinkedNodes.TryGetValue(letter, out node);
+            }
+
+
+            return TraverseAllWords(node, new List<string>());
+        }
+
+        public static IEnumerable<string> TraverseAllWords(Node node, List<string> completions) {
+            if (!node.LinkedNodes.Any())
+                return Enumerable.Empty<string>();
+            foreach (Node child in node.LinkedNodes.Values) {
+                if (child.IsTerminal) completions.Add(child.Value);
+                completions.Concat(TraverseAllWords(child, completions));
+            }
+            return completions;
+        }
+        public static void PrintTrie(List<Node> nodes) {
+
+            var childNodes = new List<Node>();
+            if (nodes == null || nodes.Count == 0)
+                return;
+
+            foreach (var node in nodes) {
+                var complete = node.IsTerminal ? "#" : " ";
+                Console.Write("'" + node.Value + "'" + complete + " ");
+                foreach (var childNode in node.LinkedNodes.Values) {
+                    childNodes.Add(childNode);
+                }
+            }
+            Console.WriteLine("********");
+            PrintTrie(childNodes);
+
+        }
+
+        public static bool AddWord(string newWord) {
+            return false;
+        }
+
+    }
+
+    class Node {
+        public string Value { get; set; }
+        public Dictionary<char, Node> LinkedNodes { get; set; }
+        public bool IsTerminal { get; set; }
+        public Node(string v, Dictionary<char, Node> childNodes = null) {
+            this.Value = v;
+            this.LinkedNodes = childNodes ?? new Dictionary<char, Node>();
+        }
+    }
+
+
+
+
+}
